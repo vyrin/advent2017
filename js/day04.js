d00fdf7b1e67f2078579749db5a40d2ffb875e97
Colin Liotta <colin@extrareal.com>
2017-12-04 12:34:01 -0800

    Day 04.

--- js/day04.js
+++ js/day04.js
@@ -3,9 +3,43 @@ advent.day04 = advent.Day.extend({
 		var input = this.input;
 		var len = input.length;
 
-		var solution = 0;
 		var parts = input.split("\n");
 
-		this.answer(1, solution);
+		var total = 0;
+		parts.forEach(part => {
+			var keys = {};
+			var sub = part.split(" ");
+			if (_.all(sub, s => {
+				if (keys[s]) {
+					return false;
+				}
+				keys[s] = true;
+				return true;
+			})) {
+				total++;
+			}
+		})
+
+		this.answer(1, total);
+
+		var total = 0;
+		parts.forEach(part => {
+			var keys = {};
+			var sub = part.split(" ");
+			if (_.all(sub, s => {
+				var arr = s.split("");
+				arr.sort();
+				s = arr.join("");
+				if (keys[s]) {
+					return false;
+				}
+				keys[s] = true;
+				return true;
+			})) {
+				total++;
+			}
+		})
+
+		this.answer(2, total);
 	},
 });
