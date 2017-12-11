663e03a4461e530fde7856c40aa712063731d462
Colin Liotta <colin@extrareal.com>
2017-12-10 21:12:38 -0800

    Day 11.

--- js/day11.js
+++ js/day11.js
@@ -4,12 +4,38 @@ advent.day11 = advent.Day.extend({
 		var len = input.length;
 
 		var solution = 0;
-		var parts = input.split("\n");
+		var parts = input.split(",");
 
 		// parts = parts.map(x => parseInt(x));
 
-		
+		var map = Array.fillMulti([200, 200], 0);
+		var pos = _v(0, 0);
 
-		this.answer(1, solution);
+		var even = {
+			"s": _v(0, -1),
+			"n": _v(0, 1),
+			"ne": _v(-1, 0.5),
+			"nw": _v(1, 0.5),
+			"se": _v(-1, -0.5),
+			"sw": _v(1, -0.5),
+		}
+
+		var foo = {};
+
+		var max = 0;
+
+		parts.forEach(step => {
+			var dir = even[step];
+			pos.addEquals(dir);
+			var dist = (Math.abs(pos.x) + Math.abs(pos.y - (pos.x / 2)))
+			if (dist > max) {
+				max = dist;
+			}
+		})
+
+		var dist = (Math.abs(pos.x) + Math.abs(pos.y - (pos.x / 2)))
+
+		this.answer(1, dist);
+		this.answer(2, max);
 	},
 });
