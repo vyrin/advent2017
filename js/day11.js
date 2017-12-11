b33a4549cecb21437eef12a70bb1fb14a3728e4e
Colin Liotta <colin@extrareal.com>
2017-12-11 10:36:15 -0800

    Cleaner, more interesting interpretation of the hex grid in day 11.

--- js/day11.js
+++ js/day11.js
@@ -8,32 +8,27 @@ advent.day11 = advent.Day.extend({
 
 		// parts = parts.map(x => parseInt(x));
 
-		var map = Array.fillMulti([200, 200], 0);
 		var pos = _v(0, 0);
 
-		var even = {
-			"s": _v(0, -1),
-			"n": _v(0, 1),
-			"ne": _v(-1, 0.5),
-			"nw": _v(1, 0.5),
-			"se": _v(-1, -0.5),
-			"sw": _v(1, -0.5),
+		var dirs = {
+			"s": _v(-1, -1),
+			"n": _v(1, 1),
+			"ne": _v(1, 0),
+			"nw": _v(0, 1),
+			"se": _v(0, -1),
+			"sw": _v(-1, 0),
 		}
 
-		var foo = {};
-
 		var max = 0;
-
+		var dist = 0;
 		parts.forEach(step => {
-			var dir = even[step];
+			var dir = dirs[step];
 			pos.addEquals(dir);
-			var dist = (Math.abs(pos.x) + Math.abs(pos.y - (pos.x / 2)))
+			dist = _.max([pos.x, pos.y]);
 			if (dist > max) {
 				max = dist;
 			}
 		})
-
-		var dist = (Math.abs(pos.x) + Math.abs(pos.y - (pos.x / 2)))
 
 		this.answer(1, dist);
 		this.answer(2, max);
