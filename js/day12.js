ca19a85f9ba9247f01c8689200480e0d72c41df3
Colin Liotta <colin@extrareal.com>
2017-12-11 21:11:30 -0800

    Day 12.

--- js/day12.js
+++ js/day12.js
@@ -8,8 +8,44 @@ advent.day12 = advent.Day.extend({
 
 		// parts = parts.map(x => parseInt(x));
 
-		
+		var k = {};
+
+		var regex = /(.*) <-> (.*)/;
+		parts.forEach(line => {
+			var m = line.match(regex);
+			var thing = {
+				name: m[1],
+				neighbors: m[2].split(", ")
+			}
+			k[m[1]] = thing;
+		})
+
+		this.visited = {};
+		this.found = {};
+
+		this.dive(k["0"], k, 0);
+		solution = _.keys(this.visited).length;
 
 		this.answer(1, solution);
+
+		this.visited = {};
+		this.found = {};
+
+		_.keys(k).forEach((name, i) => {
+			this.dive(k[name], k, i);
+		})
+
+		this.answer(2, _.keys(this.found).length);
 	},
+
+	dive : function (node, k, g) {
+		if (this.visited[node.name]) {
+			return;
+		}
+		this.found[g] = true;
+		this.visited[node.name] = true;
+		node.neighbors.forEach(n => {
+			this.dive(k[n], k, g);
+		})
+	}
 });
